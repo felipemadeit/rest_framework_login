8d752d70bb50a4c033bc3795ebb54fa2135ecb17
Julian Homez <felipemadeit@gmail.com>
2024-05-07 14:32:29 -0500

    Settings changed

--- login/settings.py
+++ login/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q@#!&@o6s49-c2ag+g_2c5p5vp*^5ezo6c*@oce4b@qtpr^q9@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
