4c7f723d639c57d548c202266b9ccdd885216863
Julian Homez <felipemadeit@gmail.com>
2024-05-07 21:59:08 -0500

    Added a message to the different responses

--- login/views.py
+++ login/views.py
@@ -14,13 +14,12 @@ from rest_framework.authentication import TokenAuthentication
 @api_view(['POST'])
 def login(request):
     
-    print(request.data)
     # Function of django.shortcuts that validate if the user is created
     user = get_object_or_404(User, username=request.data['username'])
     
     if not user.check_password(request.data['password']):
         # If the password written is false
-        return Response({"error": "Invalid Password"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"error": "Invalid Password", "message": "Incorrect password"}, status=status.HTTP_400_BAD_REQUEST)
     
     # Tuple (the token and a boolean variable called created)
     token, created = Token.objects.get_or_create(user=user)
@@ -28,7 +27,7 @@ def login(request):
     # User in json object
     serializer = user_serializer(instance=user)
     
-    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
+    return Response({"token": token.key, "user": serializer.data, "message": "Login Succefull"}, status=status.HTTP_200_OK)
 
 
 
@@ -70,9 +69,6 @@ def register(request):
 def profile(request):
     
     print(request.user)
-    
-    
-    
     
     
     return Response("You are login with: {}". format(request.user.username), status=status.HTTP_200_OK)
